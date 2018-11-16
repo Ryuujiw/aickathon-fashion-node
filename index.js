6759296d823c6957d0715b9c5f9b6359ed9dc016
Ryuujiw <32730247+Ryuujiw@users.noreply.github.com>
2018-11-16 18:37:24 +0800

    trying the bot

--- index.js
+++ index.js
@@ -5,9 +5,21 @@ const config = require('./configurations/server');
 const fashion = require('./apis/fashion');
 
 // Scraper
-const scraper = require('cheerio');
+const request = require('request');
+const cheerio = require('cheerio');
 
+const in_url = 'https://www.reddit.com/r/javascript/';
 
+let result = [];
+
+request(in_url, (err, res, body) => {
+  //Load HTML body into cheerio
+  const $ = cheerio.load(body);
+  $('.score').attr('title', (i, val) => {
+    result.push(val);
+    console.log(result)
+  });
+});
 
 // Getter from Front-end
 
