e661f4736762d83c5712c71ffb6d074df5c91d8a
Ryuujiw <32730247+Ryuujiw@users.noreply.github.com>
2018-11-17 08:32:18 +0800

    scraper now scrapes upvote and links

--- index.js
+++ index.js
@@ -2,36 +2,37 @@
 const config = require('./configurations/server');
 
 // APIs
-const fashion = require('./apis/fashion');
+const fashion = require('./apis/coqnitiics_api');
 
 // Scraper
 const request = require('request');
 const cheerio = require('cheerio');
+const PAGE_MAX = 10;
 
-
-
-request({
-    method: 'GET',
-    url: 'http://www.chictopia.com/browse/people'
-}, (err, res, body) => {
-    if (err) return console.error(err);
-
-    let $ = cheerio.load(body);
-
-    const links = [];
-    const upvotes = [];
-
-    $('.lg_photo img').each(function(i, elem) {
-      links[i] = $(this).attr('src');
-    });
-
-    $(".lg_photo div[class='white px10']").each(function(i, elem){
-        temp = $(this).text().match('[1-9][0-9]+ votes');
-        upvotes[i] = temp[0];
-    });
-    console.log(links);
-    console.log(upvotes);
-})
+for (i = 0; i < PAGE_MAX; i++) {
+    request({
+        method: 'GET',
+        url: `http://www.chictopia.com/browse/people/${i}`
+    }, (err, res, body) => {
+        if (err) return console.error(err);
+    
+        let $ = cheerio.load(body);
+    
+        const links = [];
+        const upvotes = [];
+    
+        $('.lg_photo img').each(function(i, elem) {
+          links[i] = $(this).attr('src');
+        });
+    
+        $(".lg_photo div[class='white px10']").each(function(i, elem){
+            temp = $(this).text().match('[1-9][0-9]+ votes');
+            upvotes[i] = temp[0];
+        });
+        console.log(links);
+        console.log(upvotes);
+    })
+}
 
 console.log('end');
 
